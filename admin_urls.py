9bc486477ff9784d54656c6697b0baaa23bc9933
leith.john <leith.john@5dd744ac-175b-11de-8b5c-13accb87f508>
2009-03-23 13:25:18 +0000

    moved manipulation of accounts to the admin and cleaned up the sloppy code

--- admin_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from solardev.enrollment.invoice_views import *
-
-urlpatterns = patterns('',
-    (r'invoice/(?P<invoice>\d+)/cancel-fees/', cancel_fees),
-    (r'invoice/(?P<invoice>\d+)/add-enrollment/', add_enrollment),
-)
--- urls.py
+++ urls.py
@@ -2,12 +2,7 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    url(r'^account/new/', update_account, name="accounts-add-account"),
-    url(r'^account/(\d+)/', update_account, name="accounts-change-account"),
-    url(r'^journal/', journal, name="journal"),
-    url(r'^ledger/(?P<id>\d+)/', ledger, name="ledger"), 
-    url(r'^expense/new/', add_expense, name="add_expense"),
-    url(r'^revenue/new/', add_revenue, name="add_revenue"),
-	url(r'^close/(\d+)/', close_account, name="accounts-close"),
-    url(r'^', summary, name="summary"), 
+    url(r'^journal/', journal, name="accounts-journal"),
+    url(r'^ledger/(?P<id>\d+)/', ledger, name="accounts-ledger"),
+    url(r'^', summary, name="accounts-summary"),
 )
